82721bcb0d71fe3d7316c02f7fd8e8c6a372268b
Juan <Juan@DESKTOP-09E3G42>
2019-06-27 12:09:38 -0300

    momentaneo para levantar app

--- WIS.Billing.WebSiteCore/ClientApp/src/App.js
+++ WIS.Billing.WebSiteCore/ClientApp/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Loading } from './components/Loading';
-import { ScrollContextProvider } from './components/GridComponents/ScrollContextProvider';
-import HTML5Backend from 'react-dnd-html5-backend';
-import { DragDropContext } from 'react-dnd';
-//import { Home } from './components/Home';
+//import { Loading } from './components/Loading';
+//import { ScrollContextProvider } from './components/GridComponents/ScrollContextProvider';
+//import HTML5Backend from 'react-dnd-html5-backend';
+//import { DragDropContext } from 'react-dnd';
+import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { Maintenances } from './components/Maintenances';
@@ -18,7 +18,7 @@ import { AddClient } from './components/AddClient';
 import { AddHourRate } from './components/AddHourRate';
 import { FetchHourRate } from './components/FetchHourRate';
 
-const Home = React.lazy(() => import('./components/Home'));
+//const Home = React.lazy(() => import('./components/Home'));
 
 export default class App extends Component {
     displayName = App.name
@@ -40,9 +40,8 @@ export default class App extends Component {
 
     render() {
         return (
-            <Layout setMenuOpening={this.setMenuOpening}>
-                <ScrollContextProvider>
-                    <Suspense fallback={<Loading />}>
+            <Layout >
+                
                         <Route exact path='/' component={Home} />
                         <Route path='/counter' component={Counter} />
                         <Route path='/fetchdata' component={FetchData} />
@@ -57,10 +56,9 @@ export default class App extends Component {
                         <Route path='/client/edit/:clientId' component={AddClient} />
                         <Route path='/addHourRate/' component={AddHourRate} />
                         <Route path='/fetchHourRate' component={FetchHourRate} />
-                    </Suspense>
-                </ScrollContextProvider>
+                
             </Layout>
         );
     }
 }
-export default DragDropContext(HTML5Backend)(App); //Si se necesita touch, hay que usar el backend de touch
+//export default DragDropContext(HTML5Backend)(App); //Si se necesita touch, hay que usar el backend de touch
