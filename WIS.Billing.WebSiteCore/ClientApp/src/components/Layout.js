0dcb9fdedbe8c56dbce0c1a0eb7af9f3959460f2
Juan <Juan@DESKTOP-09E3G42>
2019-07-04 17:38:54 -0300

    ok

--- WIS.Billing.WebSiteCore/ClientApp/src/components/Layout.js
+++ WIS.Billing.WebSiteCore/ClientApp/src/components/Layout.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import { Col, Grid, Row } from 'react-bootstrap';
+import { Col, Grid, Row, Container } from 'react-bootstrap';
 import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
-  displayName = Layout.name
+    displayName = Layout.name
 
-  render() {
-    return (
-      <Grid fluid>
-        <Row>
-          <Col sm={3}>
-            <NavMenu />
-          </Col>
-          <Col sm={9}>
-            {this.props.children}
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col sm={3}>
+                        <NavMenu />
+                    </Col>
+                    <Col sm={9}>
+                        {this.props.children}
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
