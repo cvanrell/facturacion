94f47be87d256a4d0c22f08310873561e12b6bc4
Juan <Juan@DESKTOP-09E3G42>
2019-07-16 11:53:46 -0300

    fix clases parciales

--- WIS.Billing.EntitiesCore/Entities/T_GRID_DEFAULT_CONFIG.cs
+++ WIS.Billing.EntitiesCore/Entities/T_GRID_DEFAULT_CONFIG.cs
@@ -8,49 +8,46 @@ namespace WIS.Billing.EntitiesCore
 
     [Table("T_GRID_DEFAULT_CONFIG")]
     public partial class T_GRID_DEFAULT_CONFIG
-    {
-        //[Key]
+    {        
         [Column(Order = 0)]
         [StringLength(30)]
         public string CD_APLICACION { get; set; }
-
-        //[Key]
+     
         [Column(Order = 1)]
         [StringLength(30)]
         public string CD_BLOQUE { get; set; }
 
-        //[Key]
         [Column(Order = 2)]
         [StringLength(30)]
-        public  string NM_DATAFIELD { get; set; }
+        public override string NM_DATAFIELD { get; set; }
 
         [StringLength(100)]
-        public  string DS_COLUMNA { get; set; }
+        public override string DS_COLUMNA { get; set; }
 
-        public  short? NU_ORDEN_VISUAL { get; set; }
+        public override short? NU_ORDEN_VISUAL { get; set; }
 
         [Required]
         [StringLength(1)]
-        public string FL_VISIBLE { get; set; }
+        public override string FL_VISIBLE { get; set; }
 
         public int? RESOURCEID { get; set; }
 
         [Required]
         [StringLength(1)]
-        public string VL_ALINEACION { get; set; }
+        public override string VL_ALINEACION { get; set; }
 
         [StringLength(30)]
         public string DS_DATA_FORMAT_STRING { get; set; }
 
-        public decimal? VL_WIDTH { get; set; }
+        public override decimal? VL_WIDTH { get; set; }
 
         [Required]
         [StringLength(2)]
-        public string VL_TYPE { get; set; }
+        public override string VL_TYPE { get; set; }
 
         [StringLength(500)]
         public string VL_LINK { get; set; }
 
-        public short? VL_POSICION_FIJADO { get; set; }
+        public override short? VL_POSICION_FIJADO { get; set; }
     }
 }
