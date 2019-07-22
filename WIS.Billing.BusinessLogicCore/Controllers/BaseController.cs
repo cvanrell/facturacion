1578b595856fc2fa44f6ae6c5a9db6b718c43370
Juan <Juan@DESKTOP-09E3G42>
2019-07-22 16:34:34 -0300

    agregada funcion con reflexion para insert de registro

--- WIS.Billing.BusinessLogicCore/Controllers/BaseController.cs
+++ WIS.Billing.BusinessLogicCore/Controllers/BaseController.cs
@@ -15,11 +15,17 @@ using WIS.CommonCore.FormComponents;
 using WIS.BusinessLogicCore.FormUtil.Validation;
 using WIS.CommonCore.App;
 using WIS.BusinessLogicCore.Controllers;
+using WIS.Billing.EntitiesCore;
+using System.Reflection;
+using System.Globalization;
 
 namespace WIS.BusinessLogicCore.Controllers
 {
     public abstract class BaseController : IController, IGridController, IFormController
     {
+        private static string DATETIME_FORMAT_CORTO = "dd/MM/yyyy";
+        private static string DATETIME_FORMAT = "dd/MM/yyyy HH:mm:ss";
+
         #region Page
         public virtual PageQueryData PageLoad(PageQueryData data, int userId)
         {
@@ -107,7 +113,21 @@ namespace WIS.BusinessLogicCore.Controllers
         }
         public virtual Grid GridCommit(IGridService service, Grid grid, GridFetchRequest query, int userId)
         {
-            throw new NotImplementedException();
+            
+            // Crear contexto y hacer el insert
+            using (WISDB context = new WISDB())
+            {
+                foreach(GridRow row in grid.Rows)
+                {
+                    string[] dataList = new string[] { "" };
+                    Client c = RowToEntity<Client>(row, dataList.ToList());
+
+                    context.Clients.Add(c);
+                    context.SaveChanges();
+                }
+                
+            }
+            return grid;                
         }
         public virtual GridMenuItemAction GridMenuItemAction(IGridService service, GridMenuItemAction selection, int userId)
         {
@@ -123,5 +143,132 @@ namespace WIS.BusinessLogicCore.Controllers
             return new GridValidationSchema();
         }
         #endregion
+
+        private static T RowToEntity<T>(GridRow row, List<string> colPropeExclude, bool IsOlds = false)
+        {
+            object Out = Activator.CreateInstance(typeof(T), new object[] { });
+            foreach (PropertyInfo prop in Out.GetType().GetProperties())
+            {
+                if (!colPropeExclude.Contains(prop.Name))
+                {
+                    string add = row.Cells[0].Column.Name;
+
+
+                    GridCell cell = row.Cells.FirstOrDefault(x => x.Column.Name == prop.Name);
+                    if (cell != null)
+                    {
+                        string inValue = cell.Value;
+                        //if (IsOlds)
+                        //    inValue = cell.old_value;
+
+                        object value = ConvertStringToPropertyType(inValue, prop.PropertyType);
+                        prop.SetValue(Out, value);
+                    }
+                }
+            }
+            return (T)Out;
+        }
+
+        protected static object ConvertStringToPropertyType(string valor, Type propertyType)
+        {
+            try
+            {
+                Object converted = null;
+                //STRING
+                if (propertyType == typeof(String))
+                    converted = valor;
+                // INT 16
+                else if (propertyType == typeof(Int16))
+                    converted = Int16.Parse(valor);
+                else if (propertyType == typeof(Int16?))
+                {
+                    if (!string.IsNullOrEmpty(valor))
+                        converted = (Int16?)Int16.Parse(valor);
+                }
+                // INT 32
+                if (propertyType == typeof(Int32))
+                    converted = Int32.Parse(valor);
+                else if (propertyType == typeof(Int32?))
+                {
+                    if (!string.IsNullOrEmpty(valor))
+                        converted = (Int32?)Int32.Parse(valor);
+                }
+                // INT 64
+                else if (propertyType == typeof(Int64))
+                    converted = Int64.Parse(valor);
+                else if (propertyType == typeof(Int64?))
+                {
+                    if (!string.IsNullOrEmpty(valor))
+                        converted = (Int64?)Int64.Parse(valor);
+                }
+                // DECIMAL
+                else if (propertyType == typeof(Decimal))
+                    converted = Decimal.Parse(valor);
+                else if (propertyType == typeof(Decimal?))
+                {
+                    if (!string.IsNullOrEmpty(valor))
+                        converted = (Decimal?)Decimal.Parse(valor);
+                }
+                // DATEIME
+                else if (propertyType == typeof(DateTime))
+                {
+                    var ci = new CultureInfo("es-UY");
+                    var formatString = "yyyy-MM-dd HH:mm:ss";
+
+                    if (valor.ToString().Length == 10)
+                        formatString = "yyyy-MM-dd";
+
+                    converted = DateTime.ParseExact(valor, formatString, ci);
+                }
+                else if (propertyType == typeof(DateTime?))
+                {
+                    if (!string.IsNullOrEmpty(valor))
+                    {
+                        Exception temp = null;
+
+                        try
+                        {
+                            var ci = new CultureInfo("es-UY");
+                            var formatString = "yyyy-MM-dd HH:mm:ss";
+
+                            if (valor.ToString().Length == 10)
+                                formatString = "yyyy-MM-dd";
+
+                            converted = DateTime.ParseExact(valor, formatString, ci);
+                        }
+                        catch (Exception ex)
+                        {
+                            temp = ex;
+                        }
+
+                        if (temp != null)
+                        {
+                            try
+                            {
+                                var ci = new CultureInfo("es-UY");
+                                var formatString = DATETIME_FORMAT;
+
+                                if (valor.ToString().Length == 10)
+                                    formatString = DATETIME_FORMAT_CORTO;
+
+                                converted = DateTime.ParseExact(valor, formatString, ci);
+                            }
+                            catch (Exception ex)
+                            {
+                                throw temp;
+                            }
+                        }
+
+
+                    }
+                }
+                return converted;
+            }
+            catch (FormatException ex)
+            {
+                //LogException(ex, "ConvertStringToPropertyType");
+                throw new Exception("Error en conversion de datos: " + ex.Message);
+            }
+        }
     }
 }
