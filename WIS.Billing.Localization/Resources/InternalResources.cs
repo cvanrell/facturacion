6ad975f7543d81874d3f2d2b1b962011d5a25e48
Juan <Juan@DESKTOP-09E3G42>
2019-07-25 17:10:00 -0300

    ok

---
+++ WIS.Billing.Localization/Resources/InternalResources.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WIS.Localization.Resources
+{
+    public class InternalResources
+    {
+        #region - Master -
+
+        public string Master_Sec0_metaTitle_MasterTitle = "LIS";
+        public string Master_Sec0_metaTitle_MasterFooter = "Copyright © 2019. Todos los derechos reservados. Sistema desarrollado por";
+
+        public string Master_Menu_lbl_STOCK_STO395 = "Stock por producto";
+        public string Master_Menu_lbl_STOCK_STO150 = "Stock por ubicación";
+        public string Master_Menu_lbl_CONTROLDOCUMENTAL = "Control Documental";
+        public string Master_Menu_lbl_CONTROLDOCUMENTAL_DOC080 = "Órdenes de Ingreso";
+        public string Master_Menu_lbl_CONTROLDOCUMENTAL_DOC020 = "Saldos de Documentos";
+        public string Master_Menu_lbl_CONTROLDOCUMENTAL_DOC095 = "Documentos";
+        public string Master_Menu_lbl_CONTROLDOCUMENTAL_DOC035 = "Detalle Ingreso";
+        public string Master_Menu_lbl_CONTROLDOCUMENTAL_DOC036 = "Detalle Egreso";
+        public string Master_Menu_lbl_CONTROLDOCUMENTAL_DOC260 = "Cambio de Estado";
+        public string Master_Menu_lbl_CONTROLDOCUMENTAL_DOC290 = "Documentos Producción";
+
+        #endregion
+
+        #region - Errores -
+
+        #endregion
+
+        #region - General -
+        public string General_Sec0_btn_CONFIRMAR = "Confirmar";
+
+        #endregion
+
+        /*
+         * ==============================PANTALLAS===============================
+         * Por favor mantener secciones y nombres de pantalla en orden alfabetico 
+         * ======================================================================
+         */
+         
+        #region - Documentacion -
+
+        
+
+        
+
+       
+
+       
+
+        #region DOC080
+
+        public string DOC080_Sec0_pageTitle_Titulo = "Ordenes de Ingreso";
+
+        public string DOC080_frm1_lbl_Legend1 = "Datos básicos 01";
+        public string DOC080_frm1_lbl_Legend2 = "Datos básicos 02";
+        public string DOC080_frm1_lbl_Legend3 = "Datos básicos 03";
+        public string DOC080_frm1_lbl_Legend4 = "Datos básicos 04";
+        public string DOC080_frm1_lbl_Legend5 = "Datos calculados";
+
+        public string DOC080_frm1_lbl_NU_DOCUMENTO = "Nro. Documento";
+        public string DOC080_frm1_lbl_TP_INGRESO = "Tipo ingreso";
+        public string DOC080_frm1_lbl_ESTADO = "Estado";
+        public string DOC080_frm1_lbl_CD_EMPRESA= "Empresa";
+        public string DOC080_frm1_lbl_CD_CLIENTE= "Proveedor";
+        public string DOC080_frm1_lbl_DS_EMPRESA= "Desc. Empresa";
+        public string DOC080_frm1_lbl_DT_ADDROW = "Alta";
+        public string DOC080_frm1_lbl_DS_PROVEEDOR= "Desc. Proveedor";
+
+        public string DOC080_frm1_lbl_TP_DUA = "DM";
+        public string DOC080_frm1_lbl_NRO_DUA = "Nro. DM";
+        public string DOC080_frm1_lbl_DT_DUA = "Fecha DM";
+        public string DOC080_frm1_lbl_DT_PROGRAMADO = "Programado";
+        public string DOC080_frm1_lbl_NU_EXPORT = "Export";
+        public string DOC080_frm1_lbl_NU_IMPORT = "Import";
+        public string DOC080_frm1_lbl_DT_UPDROW = "Modificado";
+        public string DOC080_frm1_lbl_CD_DESPACHANTE = "Despachante";
+        public string DOC080_frm1_lbl_DS_DESPACHANTE = "Desc. Despachante";
+        public string DOC080_frm1_lbl_DT_ENVIADO = "Enviado";
+
+        public string DOC080_frm1_lbl_NU_FACTURA = "Factura";
+        public string DOC080_frm1_lbl_NU_CONOCIMIENTO = "Conocimiento";
+        public string DOC080_frm1_lbl_QT_VOLUMEN = "Volumen";
+        public string DOC080_frm1_lbl_QT_BULTOS = "Bultos";
+        public string DOC080_frm1_lbl_CD_UNIDAD_MEDIDA = "Unid. Medida";
+        public string DOC080_frm1_lbl_DS_UNIDAD_MEDIDA = "Desc. Unid. Medida";
+        public string DOC080_frm1_lbl_QT_PESO = "Peso";
+        public string DOC080_frm1_lbl_VL_ARBITRAJE = "Arbitraje";
+        public string DOC080_frm1_lbl_QT_CONTENEDOR_20 = "Cont. 20";
+        public string DOC080_frm1_lbl_QT_CONTENEDOR_40 = "Cont. 40";
+        public string DOC080_frm1_lbl_TP_ALMACENAJE_SEGURO = "Tp. Almac. y seg";
+        public string DOC080_frm1_lbl_DS_ALMACENAJE_SEGURO = "Desc. Almac. y seg";
+        public string DOC080_frm1_lbl_CD_VIA = "Via";
+        public string DOC080_frm1_lbl_DS_VIA = "Desc. Via";
+        public string DOC080_frm1_lbl_CD_TRANSPORTADORA = "Transportadora";
+        public string DOC080_frm1_lbl_DS_TRANSPORTADORA = "Desc. Transportadora";
+
+        public string DOC080_frm1_lbl_DS_DOCUMENTO = "Desc. Documento";
+        public string DOC080_frm1_lbl_CD_MONEDA = "Moneda";
+        public string DOC080_frm1_lbl_DS_MONEDA = "Desc. Moneda";
+        public string DOC080_frm1_lbl_NU_DOC_TRANSPORTE = "Nro. Doc. Transporte";
+        public string DOC080_frm1_lbl_ANEXO_1 = "Anexo 1";
+        public string DOC080_frm1_lbl_VL_SEGURO= "Seguro";
+        public string DOC080_frm1_lbl_NU_AGENDA = "Agenda";
+        public string DOC080_frm1_lbl_ANEXO_2 = "Anexo 2";
+        public string DOC080_frm1_lbl_VL_FLETE = "Flete";
+        public string DOC080_frm1_lbl_CD_SITUACAO = "Situación";
+        public string DOC080_frm1_lbl_CD_FUNCIONARIO = "Funcionario";
+        public string DOC080_frm1_lbl_CD_DEPOSITO = "Depósito";
+        public string DOC080_frm1_lbl_VL_OTROS_GASTOS = "Otros gastos";
+
+        public string DOC080_frm1_lbl_TOTAL_FOB = "Fob";
+        public string DOC080_frm1_lbl_TOTAL_CIF = "Cif";
+        public string DOC080_frm1_lbl_TOTAL_CIF_LINEAS = "Cif Líneas";
+
+        public string DOC080_Sec0_btn_CONFIRMAR = "Confirmar";
+        public string DOC080_Sec0_btn_CONFIRMAR_CONTINUAR= "Confirmar y continuar";
+        public string DOC080_Sec0_btn_CANCELAR= "Cancelar";
+
+        public string DOC080_grid1_colname_CD_CAMION = "Camión";
+        public string DOC080_grid1_colname_NU_DOCUMENTO = "Nro. Doc. Transp.";
+        public string DOC080_grid1_colname_TP_DUA = "Tipo DUA";
+        public string DOC080_grid1_colname_AUXDS_DOCUMENTO = "Desc. Tipo doc.";
+        public string DOC080_grid1_colname_DS_MONEDA = "Moneda";
+        public string DOC080_grid1_colname_CD_FORNECEDOR = "Prov.";
+        public string DOC080_grid1_colname_NU_AGENDA = "Agenda";
+        public string DOC080_grid1_colname_ID_FICTICIO = "Ficticio";
+        public string DOC080_grid1_colname_VL_ARBITRAJE = "Arbitraje";
+        public string DOC080_grid1_colname_NU_EXPORT = "Export";
+        public string DOC080_grid1_colname_DT_ENVIADO = "F. Enviado";
+        public string DOC080_grid1_colname_NU_FACTURA = "Factura";
+        public string DOC080_grid1_colname_QT_BULTO = "Bulto";
+        public string DOC080_grid1_colname_CD_EMPRESA = "Cód. Emp";
+        public string DOC080_grid1_colname_NM_EMPRESA = "Empresa";
+        public string DOC080_grid1_colname_NU_IMPORT = "Import";
+        public string DOC080_grid1_colname_NU_CORRELATIVO = "Correlativo";
+        public string DOC080_grid1_colname_QT_CONTENEDOR20 = "Cont. 20";
+        public string DOC080_grid1_colname_CD_VIA = "Via";
+        public string DOC080_grid1_colname_DS_VIA = "Via";
+        public string DOC080_grid1_colname_QT_VOLUMEN = "Volumen";
+        public string DOC080_grid1_colname_VL_FLETE = "Flete";
+        public string DOC080_grid1_colname_TP_ALMACENAJE_Y_SEGURO = "T. Alm. y Seg.";
+        public string DOC080_grid1_colname_TP_DOCUMENTO = "Tipo doc.";
+        public string DOC080_grid1_colname_DS_ANEXO1 = "Anexo 1";
+        public string DOC080_grid1_colname_QT_CONTENEDOR40 = "Cont. 40";
+        public string DOC080_grid1_colname_QT_PESO = "Peso";
+        public string DOC080_grid1_colname_DS_ALMACENAJE_Y_SEGURO = "Almacenaje y Seguro";
+        public string DOC080_grid1_colname_DT_ADDROW = "Alta";
+        public string DOC080_grid1_colname_CD_UNIDAD_MEDIDA_BULTO = "U. Med.";
+        public string DOC080_grid1_colname_NU_DTI = "DTI";
+        public string DOC080_grid1_colname_DS_UNIDAD_MEDIDA_BULTO = "Unidad de medida";
+        public string DOC080_grid1_colname_CD_SITUACAO = "Sit.";
+        public string DOC080_grid1_colname_DS_RAZAO_SOCIAL = "Razón social";
+        public string DOC080_grid1_colname_VL_VALIDADO = "Validado";
+        public string DOC080_grid1_colname_VL_OTROS_GASTOS = "Otros gast.";
+        public string DOC080_grid1_colname_VL_SEGURO = "Seguro";
+        public string DOC080_grid1_colname_DS_ANEXO2 = "Anexo 2";
+        public string DOC080_grid1_colname_DS_DOCUMENTO = "Descripción";
+        public string DOC080_grid1_colname_NU_CONOCIMIENTO = "Conocimiento";
+        public string DOC080_grid1_colname_CD_DESPACHANTE = "Desp.";
+        public string DOC080_grid1_colname_CD_TRANSPORTISTA = "Transp.";
+        public string DOC080_grid1_colname_DS_TRANSPORTADORA = "Transportista";
+        public string DOC080_grid1_colname_CD_FUNCIONARIO = "Funcionario";
+        public string DOC080_grid1_colname_DS_SITUACAO = "Situación";
+        public string DOC080_grid1_colname_CD_MONEDA = "Moneda";
+        public string DOC080_grid1_colname_ID_ESTADO = "Estado";
+        public string DOC080_grid1_colname_DT_DECLARADO = "F. Declarado";
+        public string DOC080_grid1_colname_DT_UPDROW = "F. Modificación";
+        public string DOC080_grid1_colname_ID_AGENDAR_AUTOMATICAMENTE = "Agendar auto.";
+        public string DOC080_grid1_colname_DT_FACTURACION = "F. Facturación";
+        public string DOC080_grid1_colname_NM_DESPACHANTE = "Despachante";
+        public string DOC080_grid1_colname_ID_MANUAL = "Manual";
+        public string DOC080_grid1_colname_NU_DUA = "Nro. DUA";
+        public string DOC080_grid1_colname_DT_DUA = "F. DUA";
+        public string DOC080_grid1_colname_DT_DTI = "F. DTI";
+        public string DOC080_grid1_colname_DT_PROGRAMADO = "F. Programado";
+        public string DOC080_grid1_colname_DT_FINALIZADO = "F. Finalizado";
+        public string DOC080_grid1_colname_DT_MOVILIZA_CONTENEDOR = "F. Mov. Cont.";
+        public string DOC080_grid1_colname_NU_DOC_TRANSPORTE = "Nro. Doc.";
+        public string DOC080_grid1_colname_NU_CORRELATIVO_2 = "Correlativo 2";
+        public string DOC080_grid1_colname_CD_DEPOSITO = "Depósito";
+        public string DOC080_grid1_colname_ID_GENERAR_AGENDA = "Gen. Agenda";
+
+        #endregion
+
+        #region DOC081
+        public string DOC081_Sec0_pageTitle_Titulo = "Detalle de documento";
+                           
+        public string DOC081_frm1_lbl_NU_DOCUMENTO = "Nro. Documento";
+        public string DOC081_frm1_lbl_TP_DOCUMENTO = "Tipo ingreso";
+        public string DOC081_frm1_lbl_AUXDS_DOCUMENTO = "Descripción";
+        public string DOC081_frm1_lbl_CD_EMPRESA = "Empresa";
+        public string DOC081_frm1_lbl_NM_EMPRESA = "Descripción";
+        public string DOC081_frm1_lbl_TP_DUA = "DM";
+        public string DOC081_frm1_lbl_NU_DUA = "Nro. DM";
+        public string DOC081_frm1_lbl_NU_EXPORT = "Export.";
+        public string DOC081_frm1_lbl_NU_IMPORT = "Import.";
+        public string DOC081_frm1_lbl_DS_DOCUMENTO = "Descripción";
+        public string DOC081_frm1_lbl_NU_FACTURA = "Factura";
+        public string DOC081_frm1_lbl_NU_CONOCIMIENTO = "Conocimiento";
+        public string DOC081_frm1_lbl_ID_ESTADO = "Estado";
+        public string DOC081_frm1_lbl_DT_ADDROW = "Alta";
+        public string DOC081_frm1_lbl_DT_UPDROW = "F. Modificación";
+        public string DOC081_frm1_lbl_DT_PROGRAMADO = "Programado";
+        public string DOC081_frm1_lbl_DT_ENVIADO = "Enviado";
+        public string DOC081_frm1_lbl_QT_VALIDAR_LINEAS = "Cantidad total de lineas ingresadas";
+        public string DOC081_frm1_lbl_QT_VALIDAR_VALOR = "Valor de mercaderia total ingresado";
+
+        public string DOC081_Sec0_btn_Validar = "Validar";
+
+        public string DOC081_grid1_colname_CD_EMPRESA = "Empresa";
+        public string DOC081_grid1_colname_VL_MERCADERIA = "Valor Mercaderia";
+        public string DOC081_grid1_colname_CD_PRODUTO = "Producto";
+        public string DOC081_grid1_colname_CD_SITUACAO = "Situación";
+        public string DOC081_grid1_colname_DS_PRODUTO = "Descripción";
+        public string DOC081_grid1_colname_DS_PRODUTO_INGRESO = "Desc. ingreso";
+        public string DOC081_grid1_colname_DT_ADDROW = "Alta";
+        public string DOC081_grid1_colname_DT_DISPONIBLE = "Fecha disponible";
+        public string DOC081_grid1_colname_DT_UPDROW = "F. Modificación";
+        public string DOC081_grid1_colname_ID_DISPONIBLE = "Disponible";
+        public string DOC081_grid1_colname_NU_DOCUMENTO = "Nro. Documento";
+        public string DOC081_grid1_colname_NU_IDENTIFICADOR = "Serie/Lote";
+        public string DOC081_grid1_colname_QT_DESAFECTADA = "Desafectado";
+        public string DOC081_grid1_colname_QT_DESCARGADA = "Descargado";
+        public string DOC081_grid1_colname_QT_INGRESADA = "Ingresado";
+        public string DOC081_grid1_colname_QT_RESERVADA = "Reservado";
+        public string DOC081_grid1_colname_TP_DOCUMENTO = "Tipo documento";
+        public string DOC081_grid1_colname_VL_CIF = "CIF";
+        public string DOC081_grid1_colname_CD_FAIXA = "Faixa";
+
+        #endregion        
+
+        #endregion
+
+        #region - Stock -
+
+       
+
+        
+
+        #region STO150
+        public string STO150_Sec0_pageTitle_Titulo = "Stock";
+
+        public string STO150_frm1_lbl_Legend = "";
+
+        public string STO150_grid1_colname_QT_TRANSITO_ENTRADA = "Cant. Transito Entrada";
+        public string STO150_grid1_colname_CD_PRODUTO = "Producto";
+        public string STO150_grid1_colname_CD_FAIXA = "Embalaje";
+        public string STO150_grid1_colname_CD_ENDERECO = "Ubicación";
+        public string STO150_grid1_colname_CD_EMPRESA = "Cd. Empresa";
+        public string STO150_grid1_colname_QT_RESERVA_SAIDA = "Cant. Reserva Salida";
+        public string STO150_grid1_colname_QT_ESTOQUE = "Cant. Stock";
+        public string STO150_grid1_colname_NU_IDENTIFICADOR = "Serie/Lote";
+        public string STO150_grid1_colname_ID_INVENTARIO = "Inventario";
+        public string STO150_grid1_colname_ID_CTRL_CALIDAD = "Ctrl. Calidad";
+        public string STO150_grid1_colname_ID_AVERIA = "Id. Averia";
+
+        public string STO150_grid1_colname_TP_MANEJO_FECHA = "Tp. Manejo Fecha";
+        public string STO150_grid1_colname_CD_CLASSE = "Clase";
+        public string STO150_grid1_colname_CD_FAMILIA_PRODUTO = "Familia Producto";
+        public string STO150_grid1_colname_CD_MERCADOLOGICO = "Mercadologico";
+        public string STO150_grid1_colname_CD_PRODUTO_EMPRESA = "Producto Empresa";
+        public string STO150_grid1_colname_CD_ROTATIVIDADE = "Rotatividad";
+        public string STO150_grid1_colname_CD_SITUACAO = "Situación";
+        public string STO150_grid1_colname_CD_TIPO_ENDERECO = "Tipo Ubicacion";
+        public string STO150_grid1_colname_DS_AREA_ARMAZ = "Desc. Area";
+        public string STO150_grid1_colname_DS_PRODUTO = "Desc. Producto";
+        public string STO150_grid1_colname_DS_REDUZIDA = "Desc. Reducida";
+        public string STO150_grid1_colname_DT_VALIDADE = "F. Validar";
+        public string STO150_grid1_colname_DT_VENCIMIENTO = "F. Vencimiento";
+        public string STO150_grid1_colname_NM_EMPRESA = "Empresa";
+        public string STO150_grid1_colname_CD_AREA_ARMAZ = "Area";
+        #endregion
+
+        #region STO150-2
+        public string STO1502_Sec0_pageTitle_Titulo = "Stock";
+
+        public string STO1502_frm1_lbl_Legend = "";
+
+        #endregion
+
+        #endregion
+    }
+}
